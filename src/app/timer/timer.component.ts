aa7723c16899d96b9b2289d19098f7c0418e76b5
iori-sutani <io.sutani@gmail.som>
2025-06-05 17:24:42 +0900

    機能の追加と見た目の修正

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-timer',
@@ -8,25 +9,61 @@ import { Component } from '@angular/core';
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.css'],
 })
-export class TimerComponent {
+export class TimerComponent implements OnDestroy {
   minutes: number = 25;
   seconds: number = 0;
   isWorkPhase: boolean = true;
 
+  private timerSubscription?: Subscription;
+  private isRunning = false;
+
+  ngOninit() {
+    this.resetTimer();
+  }
+
   startTimer() {
-    console.log("Starting timer...");
-    this.isWorkPhase = true;
+    if (this.isRunning) return;
+    this.isRunning = true;
+
+    this.timerSubscription = interval(1000).subscribe(() => {
+      if (this.seconds === 0) {
+        if (this.minutes === 0) {
+          this.switchPhase();
+        }
+        else {
+          this.minutes--;
+          this.seconds = 59;
+        }
+      }
+      else {
+        this.seconds--;
+      }
+    });
   }
 
   pauseTimer() {
-    console.log("Pausing timer...");
-    this.isWorkPhase = false;
+    this.timerSubscription?.unsubscribe();
+    this.isRunning = false;
   }
 
   resetTimer() {
-    console.log("Resetting timer...");
+    this.pauseTimer();
+    this.isWorkPhase = true;
     this.minutes = 25;
     this.seconds = 0;
-    this.isWorkPhase = true;
+    this.isRunning = false;
+  }
+
+  switchPhase() {
+    this.pauseTimer();
+    this.isWorkPhase = !this.isWorkPhase;
+    this.minutes = this.isWorkPhase ? 25: 5;
+
+    this.seconds = 0;
+    this.startTimer();
+  }
+
+  ngOnDestroy()  {
+    this.pauseTimer();
   }
 }
